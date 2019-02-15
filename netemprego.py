00a790fb1624a18c77cc851ecbe630753c4f58fe
Josué Freire <josuefreire18@gmail.com>
2019-02-15 22:29:06 +0000

    Added single page protect
    added a single page protection

--- netemprego.py
+++ netemprego.py
@@ -7,7 +7,7 @@ i = 1
 
 while i < maxsize +1:
 
-	myurl = 'https://www.net-empregos.com/listagem_livre2_2.asp?page='+ str(i) +'&CATEGORIA=38%2C+37%2C+35%2C+5%2C+49&ZONA=2&cidade=Maia'
+	myurl = 'https://www.net-empregos.com/listagem_livre2_2.asp?page=' + str(i) + '&CHAVES=maia&ZONA=2&CATEGORIA=38'
 
 	uClient = uRed(myurl)
 	page_html =uClient.read()
@@ -15,12 +15,17 @@ while i < maxsize +1:
 	page_soup = soup(page_html, "html.parser")
 
 	containers = page_soup.findAll("font",{"style":"FONT-SIZE: 15px; LINE-HEIGHT: 14px"})
-	pag = page_soup.find("table", {"height": "6","width": "0"})
-	maxsize = len(pag.get_text().replace('\n','').replace(' ',''))
-	print("Page:", i)
-	
 	tam = len(containers)
+	pag = page_soup.find("table", {"height": "6","width": "0"})
+
+	print("Pag:", i, "Items:", tam)
+
+	if pag == None:
+		i=3
+	else:
+		maxsize = len(pag.get_text().replace('\n','').replace(' ',''))
+		i+=1
+
 	for y in range(tam):
-		print(containers[y].a.get_text())
-	i+=1
+		print(containers[y].get_text())
 input()
