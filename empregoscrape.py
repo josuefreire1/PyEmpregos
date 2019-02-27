1942812005dd7f0ee0a4ec0f609b4df8d6464897
Josué Freire <josuefreire18@gmail.com>
2019-02-27 20:25:04 +0000

    added the link

--- empregoscrape.py
+++ empregoscrape.py
@@ -13,14 +13,15 @@ titles = main_block[1].findAll("font",{"style":"FONT-SIZE: 15px; LINE-HEIGHT: 14
 blocks = main_block[1].findAll("table",{"width":"738"})
 blocks_size = len(blocks)
 
-for block in range(blocks_size):
+print(titles[1].find('a').get('href'))
 
+for block in range(blocks_size):
     title = titles[block].text.strip()
     company = blocks[block].findAll('td')[3].text.strip()
     date = blocks[block].findAll('td')[5].text.strip()
     catgory = blocks[block].findAll('td')[7].text.strip()
-
-    print (date,title,company,catgory, '\n\n')  
+    link = linker + titles[block].find('a').get('href')
+    print (date,title,company,catgory,link, '\n\n')  
 
 
 # for tier in tiers:
