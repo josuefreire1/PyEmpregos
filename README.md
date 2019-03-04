42e226a6ea6031c0bf5ee58635db808ec2cc7196
Josué Freire <josuefreire18@gmail.com>
2019-03-04 22:35:34 +0000

    Added functionality: reads from data.txt the categories for the jobs

--- README.md
+++ README.md
@@ -6,4 +6,23 @@ Created to have an easier way to watch all job offers, in a single page, in the 
 
 Right now this program retrieves jobs info from net-empregos and prints on the console  for the category that was selected  
 
-**To Build (you are going to need)**: python-3.7.2 and beautifulsoup4 and lxml
+**To Build (you are going to need)**: python-3.7.2 and beautifulsoup4 and csv
+
+
+## How to run
+
+Change the data.txt to the search terms that you want:
+
+```
+QA Engineer
+QA Analyst
+Quality Assurance Engineer
+```
+
+Then run the script:
+
+```
+python empregossraper.py
+```
+
+At the end of execution you should see a file named scrape.csv
--- empregoscraper.py
+++ empregoscraper.py
@@ -2,42 +2,49 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-csv_file = open('cms_scrape.csv', 'w',  newline='', encoding='utf-8')
+csv_file = open('scrape.csv', 'w',  newline='', encoding='utf-8')
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['DATE', 'TITLE', 'COMPANY', 'CATGORY', 'LINK'])
+data_category = open('data.txt', 'r')
+lines = data_category.read().replace(' ','+').split('\n')
+
 
 #Variables to control the current page
-page_index = 1
-page_limit = 2
 
-while page_index <= page_limit:
-    url = 'https://www.net-empregos.com/listagem_livre2_2.asp?page='+ str(page_index) +'&CHAVES=Maia&ZONA=2&CATEGORIA=5%2C+38%2C+34%2C+37%2C+35%2C+36%2C+49'
-    linker = 'https://www.net-empregos.com'
+for line  in lines:
+    page_index = 1
+    page_limit = 2
+    while page_index <= page_limit:
+        url = 'https://www.net-empregos.com/listagem_livre2_2.asp?page='+ str(page_index) + '&CHAVES=' + line + '&ZONA=2&CATEGORIA=5%2C+38%2C+34%2C+37%2C+35%2C+36%2C+49'
+        linker = 'https://www.net-empregos.com'
 
 
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.text, 'html.parser')
 
-    #Getting the number of pages
-    page_info = soup.find("center")
-    page_limit = len(page_info.get_text().replace('\n','').replace(' ',''))
+        #Getting the number of pages and if it's "None" then there is no more pages or only one page
 
-    #Getting the sectons where the job info is
-    main_block = soup.findAll("div",{"style":"background-color:#FFFFFF;border:1px solid #C3D9FF"})
-    titles = main_block[1].findAll("font",{"style":"FONT-SIZE: 15px; LINE-HEIGHT: 14px"})
-    blocks = main_block[1].findAll("table",{"width":"738"})
-    blocks_size = len(blocks)
+        page_info = soup.find("center")
+        if page_info != None:
+            page_limit = len(page_info.get_text().replace('\n','').replace(' ',''))
+            page_index+=1
+        else:
+            page_index = page_limit+1
 
-    #For loop that iterates betwen the the job offers also writes all the data to the csv file
-    for block in range(blocks_size):
-        title = titles[block].text.strip()
-        company = blocks[block].findAll('td')[3].text.strip()
-        date = blocks[block].findAll('td')[5].text.strip()
-        catgory = blocks[block].findAll('td')[7].text.strip()
-        link = linker + titles[block].find('a').get('href')
-        csv_writer.writerow([date,title,company,catgory,link])
+        #Getting the sectons where the job info is
+        main_block = soup.findAll("div",{"style":"background-color:#FFFFFF;border:1px solid #C3D9FF"})
+        titles = main_block[1].findAll("font",{"style":"FONT-SIZE: 15px; LINE-HEIGHT: 14px"})
+        blocks = main_block[1].findAll("table",{"width":"738"})
+        blocks_size = len(blocks)
 
-    #change the current page
-    page_index+=1
+        #For loop that iterates betwen the the job offers also writes all the data to the csv file
+        for block in range(blocks_size):
+            title = titles[block].text.strip()
+            company = blocks[block].findAll('td')[3].text.strip()
+            date = blocks[block].findAll('td')[5].text.strip()
+            catgory = blocks[block].findAll('td')[7].text.strip()
+            link = linker + titles[block].find('a').get('href')
+            csv_writer.writerow([date,title,company,catgory,link])
+
 
 csv_file.close()
